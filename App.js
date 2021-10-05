42d7e5a5dc2e4fd4fb41ee677491d232591ebff6
Madi-S <khovansky99@gmail.com>
2021-10-05 21:14:22 +0600

    AppNavigation initialized

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
-import { AppLoading } from 'expo'
+import AppLoading from 'expo-app-loading'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import {} from 'react-native'
+
 import { boostrap } from './src/bootstrap'
+import { AppNavigation } from './src/navigation/AppNavigation'
 
 export default function App() {
     const [isReady, setIsReady] = useState(false)
@@ -18,9 +20,9 @@ export default function App() {
     }
 
     return (
-        <View>
-            <Text>Open up App.js to start working on your app!</Text>
+        <>
+            <AppNavigation />
             <StatusBar style='auto' />
-        </View>
+        </>
     )
 }
