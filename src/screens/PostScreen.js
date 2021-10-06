aabb666fd42a67586edb5622facf85a4d2a9b4ee
Madi-S <khovansky99@gmail.com>
2021-10-06 17:36:01 +0600

    PostScreen styling enhanced

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,12 +1,43 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    ScrollView,
+    Button,
+    Alert
+} from 'react-native'
+
+import { THEME } from '../theme'
+import { DATA } from '../data'
 
 export const PostScreen = ({ navigation }) => {
     const postId = navigation.getParam('postId')
-    console.log('postId', postId)
+    const post = DATA.find(p => p.id === postId)
+
+    const removePost = () => {
+        Alert.alert('Post deletion', 'Are you sure?', [
+            { text: 'Cancel', style: 'cancel' },
+            {
+                text: 'Remove',
+                style: 'destructive',
+                onPress: () => console.log('OK Pressed')
+            }
+        ])
+    }
+
     return (
         <View style={styles.center}>
-            <Text>{postId}</Text>
+            <Image style={styles.image} source={{ uri: post.img }} />
+            <ScrollView style={styles.textWrapper}>
+                <Text style={styles.title}>{post.text}</Text>
+                <Button
+                    title='Delete'
+                    onPress={removePost}
+                    color={THEME.DANGER_COLOR}
+                />
+            </ScrollView>
         </View>
     )
 }
@@ -30,5 +61,15 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    image: {
+        width: '100%',
+        height: 200
+    },
+    textWrapper: {
+        padding: 10
+    },
+    title: {
+        fontFamily: 'open-regular'
     }
 })
