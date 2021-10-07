690d7abbbdf9588ff6d5083b80dbc14baa083315
Madi-S <khovansky99@gmail.com>
2021-10-07 08:38:33 +0600

    Adaptive BottomTabNavigator implemented

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -4,6 +4,7 @@ import { Ionicons } from '@expo/vector-icons'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 
 import { THEME } from '../theme'
 import { MainScreen } from '../screens/MainScreen'
@@ -48,10 +49,11 @@ const BookedNavigator = createStackNavigator(
     }
 )
 
-const bottomNavigator = createBottomTabNavigator({
+const bottomTabsConfig = {
     Post: {
         screen: PostNavigator,
         navigationOptions: {
+            tabBarLabel: 'All',
             tabBarIcon: info => (
                 <Ionicons name='ios-albums' color={info.tintColor} size={24} />
             )
@@ -60,15 +62,27 @@ const bottomNavigator = createBottomTabNavigator({
     Booked: {
         screen: BookedNavigator,
         navigationOptions: {
+            tabBarLabel: 'Bookmarked',
             tabBarIcon: info => (
                 <Ionicons name='ios-star' color={info.tintColor} size={24} />
             )
         }
     }
-}, {
-    tabBarOptions: {
-        activeTintColor: THEME.PRIMARY_COLOR
-    }
-})
+}
+
+const bottomNavigator =
+    Platform.OS === 'android'
+        ? createMaterialBottomTabNavigator(bottomTabsConfig, {
+              activeTintColor: 'white',
+              shifting: true,
+              barStyle: {
+                  backgroundColor: THEME.PRIMARY_COLOR
+              }
+          })
+        : createBottomTabNavigator(bottomTabsConfig, {
+              tabBarOptions: {
+                  activeTintColor: THEME.PRIMARY_COLOR
+              }
+          })
 
 export const AppNavigation = createAppContainer(bottomNavigator)
