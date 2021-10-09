76f942238873790002f58e51ae151c0cbf2fad11
Madi-S <khovansky99@gmail.com>
2021-10-09 10:23:31 +0600

    Mock data dependency removed

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
+import { View, StyleSheet, FlatList, Text } from 'react-native'
 
 import { Post } from './Post'
 
 export const PostList = ({ data, onOpen }) => {
+    if (!data.length) {
+        return (
+            <View style={styles.wrapper}>
+                <Text style={styles.noItems}>There are no posts yet</Text>
+            </View>
+        )
+    }
+
     return (
         <View style={styles.wrapper}>
             <FlatList
@@ -20,5 +28,11 @@ export const PostList = ({ data, onOpen }) => {
 const styles = StyleSheet.create({
     wrapper: {
         padding: 10
+    },
+    noItems: {
+        fontFamily: 'open-bold',
+        textAlign: 'center',
+        marginVertical: 10,
+        fontSize: 24
     }
 })
--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,10 +1,9 @@
-import { DATA } from '../../data'
 import { LOAD_POSTS, TOGGLE_BOOKED, REMOVE_POST, ADD_POST } from '../types'
 
 export const loadPosts = () => {
     return {
         type: LOAD_POSTS,
-        payload: DATA
+        payload: []
     }
 }
 
